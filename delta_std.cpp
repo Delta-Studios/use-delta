4546ce207ebd68fa8719f9506beaad8d117ac50a
Anonymus_12321 <61108504+AP-Studios@users.noreply.github.com>
2022-02-12 12:11:59 +0100

    std

---
+++ delta_std.cpp
@@ -0,0 +1,50 @@
+#include "delta_std.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string>
+
+//DER USER DARF KEIN STD STRING MEHR BENUTZEN KÖNNEN
+
+class delta::console {
+    public:
+    static int log(std::string args, ...) {
+        va_list arg;
+        int done;
+        va_start (arg, args.c_str());
+        done = vfprintf (stdout, args.c_str(), arg);
+        va_end (arg);
+        return 0;
+    }
+};
+
+static int put(std::string args, ...) {
+    delta::console::log((args+"\n").c_str());
+    return 0;
+};
+
+class delta::str {
+    public:
+    std::string value;
+
+    std::string getValue() {
+        return value;
+    }
+    //Eigene Funktionen z.B. trim oder so
+};
+class delta::bin {
+    public:
+    bool value;
+
+    bool getValue() {
+        return value;
+    }
+};
+class delta::num {
+    public:
+    int value;
+
+    int getValue() {
+        return value;
+    }
+};
